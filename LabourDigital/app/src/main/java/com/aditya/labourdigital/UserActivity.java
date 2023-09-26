b97bd34b371891e9c4f322cb5da4702da6b2e506
Aditya Sharma <20BCS3512@CUCHD.IN>
2023-09-26 11:35:51 +0530

    Added Firebase Realtime DB Connectivity

--- LabourDigital/app/src/main/java/com/aditya/labourdigital/UserActivity.java
+++ LabourDigital/app/src/main/java/com/aditya/labourdigital/UserActivity.java
@@ -1,22 +1,33 @@
 package com.aditya.labourdigital;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class UserActivity extends AppCompatActivity {
+
+    EditText ed1,ed2,ed3,ed4;
 
     Spinner spiner;
     FirebaseAuth auth;
@@ -24,6 +35,8 @@ public class UserActivity extends AppCompatActivity {
     TextView tv;
     FirebaseUser user;
     ProgressBar pb;
+    DatabaseReference db;
+    String value;
 
     String[] loca = {"Himachal","Punjab","Haryana"};
 
@@ -41,6 +54,16 @@ public class UserActivity extends AppCompatActivity {
         spiner = findViewById(R.id.spinner);
         sub = findViewById(R.id.button6);
         pb = findViewById(R.id.progressBar2);
+        db = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        //user Details
+        ed1 = findViewById(R.id.editTextText);
+        ed2 = findViewById(R.id.editTextText2);
+        ed3 = findViewById(R.id.editTextPhone);
+        ed4 = findViewById(R.id.editTextTextPostalAddress);
+
+
+
 
         // setting drop down menu for locations
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, loca);
@@ -49,7 +72,7 @@ public class UserActivity extends AppCompatActivity {
         spiner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String value = adapterView.getItemAtPosition(i).toString();
+                 value = adapterView.getItemAtPosition(i).toString();
                 Toast.makeText(UserActivity.this, value, Toast.LENGTH_SHORT).show();
             }
 
@@ -86,8 +109,39 @@ public class UserActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 pb.setVisibility(View.VISIBLE);
+                // get user detail as string
+
+                String nameofuser = ed1.getText().toString();
+                String DOB = ed2.getText().toString();
+                String phonenumber = ed3.getText().toString();
+                String address = ed4.getText().toString();
+                String currMail = tv.getText().toString();
+                Log.d("Values",nameofuser+" "+DOB+" "+phonenumber+" "+address+" "+value);
 
 
+
+                HashMap<String, String> hashmap = new HashMap();
+
+                hashmap.put("Name",nameofuser);
+                hashmap.put("DOB",DOB);
+                hashmap.put("Phone",phonenumber);
+                hashmap.put("Address",address);
+                hashmap.put("Work",value);
+
+                db.child(currMail.replace('.', ',')).setValue(hashmap).addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                    @Override
+                    public void onSuccess(Void unused) {
+                        pb.setVisibility(View.GONE);
+                        Toast.makeText(UserActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pb.setVisibility(View.GONE);
+                        Toast.makeText(UserActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
